032e75fb0c749c2388ce2d600f0d03ca6be31777
knowlet <knowlet3389@gmail.com>
2015-03-23 16:33:25 +0800

    init

---
+++ AVLTree/AVLTree.c
@@ -0,0 +1,266 @@
+#include "AVLTree.h"
+
+treeNode* newNode(int key)
+{
+    treeNode* pNode;
+    pNode = malloc(sizeof(treeNode));
+    pNode->bf = 0;
+    pNode->key = key;
+    pNode->left = NULL;
+    pNode->right = NULL;
+    return pNode;
+}
+
+void leftRotation(treeNode** parent, bool* unbalanced)
+{
+    treeNode* grandChild;
+    treeNode* child = (*parent)->left;
+    if (child->bf == 1) {
+        // LL
+        (*parent)->left = child->right;
+        child->right = *parent;
+        (*parent)->bf = 0;
+        *parent = child;
+    }
+    else {
+        // LR
+        grandChild = child->right;
+        child->right = grandChild->left;
+        grandChild->left = child;
+        (*parent)->left = grandChild->right;
+        grandChild->right = *parent;
+        switch (grandChild->bf) {
+            case 1:
+                (*parent)->bf = -1;
+                child->bf = 0;
+                break;
+                
+            case 0:
+                (*parent)->bf = child->bf = 0;
+                break;
+                
+            case -1:
+                (*parent)->bf = 0;
+                child->bf = 1;
+        }
+        *parent = grandChild;
+    }
+    (*parent)->bf = 0;
+    *unbalanced = false;
+}
+
+void rightRotation(treeNode** parent, bool* unbalanced)
+{
+    treeNode* grandChild;
+    treeNode* child = (*parent)->right;
+    if (child->bf == -1) {
+        // RR
+        (*parent)->right = child->left;
+        child->left = *parent;
+        (*parent)->bf = 0;
+        *parent = child;
+    }
+    else {
+        // RL
+        grandChild = child->left;
+        child->left = grandChild->right;
+        grandChild->right = child;
+        (*parent)->right = grandChild->left;
+        grandChild->left = *parent;
+        switch (grandChild->bf) {
+            case -1:
+                (*parent)->bf = 1;
+                child->bf = 0;
+                break;
+                
+            case 0:
+                (*parent)->bf = child->bf = 0;
+                break;
+                
+            case 1:
+                (*parent)->bf = 0;
+                child->bf = -1;
+        }
+        *parent = grandChild;
+    }
+    (*parent)->bf = 0;
+    *unbalanced = false;
+}
+
+void avlInsert(treeNode** parent, int key, bool* unbalanced)
+{
+    if (!*parent) {
+        *parent = newNode(key);
+        *unbalanced = true;
+    }
+    else if (key < (*parent)->key) {
+        avlInsert(&(*parent)->left, key, unbalanced);
+        if (*unbalanced)
+            switch ((*parent)->bf) {
+                case -1:
+                    (*parent)->bf = 0;
+                    *unbalanced = false;
+                    break;
+                    
+                case 0:
+                    (*parent)->bf = 1;
+                    break;
+                    
+                case 1:
+                    leftRotation(parent, unbalanced);
+            }
+    }
+    else if (key > (*parent)->key) {
+        avlInsert(&(*parent)->right, key, unbalanced);
+        if (*unbalanced)
+            switch ((*parent)->bf) {
+                case 1:
+                    (*parent)->bf = 0;
+                    *unbalanced = false;
+                    break;
+                    
+                case 0:
+                    (*parent)->bf = -1;
+                    break;
+                    
+                case -1:
+                    rightRotation(parent, unbalanced);
+            }
+    }
+    else {
+        *unbalanced = false;
+        puts("The key is already in the tree!");
+    }
+}
+
+bool isLeaf(treeNode* p)
+{
+    return p->left == NULL && p->right == NULL;
+}
+
+int delMinNode(treeNode** p)
+{
+    treeNode* prev = *p;
+    treeNode* node = *p;
+    int key = 0;
+    if ((*p)->left) {
+        treeNode* next = (*p)->left;
+        while (next != NULL){
+            prev = node;
+            node = next;
+            next = next->right;
+        }
+        key = node->key;
+        if (prev == *p) {
+            free(prev->left);
+            prev->left = NULL;
+        }
+        else {
+            free(prev->right);
+            prev->right = NULL;
+        }
+    }
+    else {
+        treeNode* next = (*p)->right;
+        while (next != NULL){
+            prev = node;
+            node = next;
+            next = next->right;
+        }
+        key = node->key;
+        if (prev != *p) {
+            free(prev->left);
+            prev->left = NULL;
+        }
+        else {
+            free(prev->right);
+            prev->right = NULL;
+        }
+    }
+    return key;
+}
+
+void avlErase(treeNode** parent, int key, bool* unbalanced)
+{
+    if (!*parent) {
+        *unbalanced = false;
+        puts("The key is not in the tree!");
+        return;
+    }
+    else if (key < (*parent)->key) {
+        avlErase(&(*parent)->left, key, unbalanced);
+        if (*unbalanced)
+            switch ((*parent)->bf) {
+                case 1:
+                    (*parent)->bf = 0;
+                    break;
+                    
+                case 0:
+                    (*parent)->bf = -1;
+                    *unbalanced = false;
+                    break;
+                    
+                case -1:
+                    rightRotation(parent, unbalanced);
+                    *unbalanced = true;
+            }
+    }
+    else if (key > (*parent)->key) {
+        avlErase(&(*parent)->right, key, unbalanced);
+        if (*unbalanced)
+            switch ((*parent)->bf) {
+                case -1:
+                    (*parent)->bf = 0;
+                    break;
+                    
+                case 0:
+                    (*parent)->bf = 1;
+                    *unbalanced = false;
+                    break;
+                    
+                case 1:
+                    leftRotation(parent, unbalanced);
+                    *unbalanced = true;
+            }
+    }
+    else if (isLeaf(*parent)) {
+        free(*parent);
+        *parent = NULL;
+        *unbalanced = true;
+    }
+    else
+        (*parent)->key = delMinNode(parent);
+}
+
+void freeTree(treeNode* root)
+{
+    if (root == NULL) return;
+    freeTree(root->left);
+    freeTree(root->right);
+    root->left = root->right = NULL;
+    root->key = 0;
+    free(root);
+}
+
+void infixOrder(treeNode* root)
+{
+    if(root==NULL) return;
+    infixOrder(root->left);
+    printf("%d ", root->key);
+    infixOrder(root->right);
+}
+void prefixOrder(treeNode* root)
+{
+    if(root==NULL) return;
+    printf("%d ", root->key);
+    prefixOrder(root->left);
+    prefixOrder(root->right);
+}
+
+void postfixOrder(treeNode* root)
+{
+    if(root==NULL) return;
+    postfixOrder(root->left);
+    postfixOrder(root->right);
+    printf("%d ", root->key);
+}
--- AVLTree/main.c
+++ AVLTree/main.c
@@ -1,15 +1,112 @@
-//
-//  main.c
-//  AVLTree
-//
-//  Created by KNowlet on 2015/3/19.
-//  Copyright (c) 2015年 KNowlet. All rights reserved.
-//
-
 #include <stdio.h>
+#include <ctype.h>
+#include "AVLTree.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int maxHeight(treeNode *p) {
+    if (!p) return 0;
+    int left_height = maxHeight(p->left);
+    int right_height = maxHeight(p->right);
+    return (left_height > right_height) ? left_height + 1 : right_height + 1;
+}
+
+int _printT(treeNode *tree, bool isLeft, int offset, int level, char s[25][81])
+{
+    static const int W = 5; // 3 + 2, -99 ~ 999
+    char key[W+1];
+    if (!tree) return 0;
+    sprintf(key, "(%03d)", tree->key);
+    int left  = _printT(tree->left, true, offset, level + 1, s);
+    int right = _printT(tree->right, false, offset+left+W, level+1, s);
+    for (int i = 0; i < W; ++i)
+        s[2*level][offset+left+i] = key[i];
+    if (level) {
+        if (isLeft) {
+            for (int i = 0; i < W+right; ++i)
+                s[2*level-1][offset+left+W/2+i] = '-';
+            s[2*level-1][offset+left+W/2] = '/';    // L
+            s[2*level-1][offset+left+W+right+W/2] = '*';
+        }
+        else {
+            for (int i = 0; i < left+W-1; ++i)
+                s[2*level-1][offset-W/2+i] = '-';
+            s[2*level-1][offset+left+W/2] = '\\';    // R
+            s[2*level-1][offset-W/2-1] = '*';
+        }
+    }
+    return left + W + right;
+}
+
+void printT(treeNode *tree)
+{
+    char s[25][81];
+    int height = maxHeight(tree) * 2;
+    if (!height) {
+        puts("Empty!");
+        return;
+    }
+    for (int i = 0; i < 24; i++)
+        sprintf(s[i], "%80s", " ");
+    _printT(tree, 0, 0, 0, s);
+    puts("");
+    for (int i = 0; i < height; ++i)
+        printf("%s\n", s[i]);
+}
+
+void horizonDisplay(treeNode* root, treeNode* ptr, int level)
+{
+    int i;
+    if (ptr!=NULL) {
+        horizonDisplay(root, ptr->right, level + 1);
+        printf("\n");
+        if (ptr == root)
+            printf("--> ");
+        for (i = 0; i < level && ptr != root; i++)
+            printf("\t");
+        printf("%d", ptr->key);
+        horizonDisplay(root, ptr->left, level + 1);
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    treeNode* avlTree = NULL;
+    bool unbalanced = false;
+    char code = 0;
+    int key = 0;
+    while (toupper(code) != 'X') {
+        printf("Enter 'I' to insert node, 'E' to erase node, 'C' to clear the tree, 'D' to display tree, 'X' to exit: ");
+        scanf("%s", &code);
+        switch (toupper(code)) {
+            case 'I':
+                printf("Enter the value of node: ");
+                scanf("%d", &key);
+                avlInsert(&avlTree, key, &unbalanced);
+                break;
+                
+            case 'E':
+                printf("Enter the value of node: ");
+                scanf("%d", &key);
+                avlErase(&avlTree, key, &unbalanced);
+                break;
+                
+            case 'C':
+                freeTree(avlTree);
+                avlTree = NULL;
+                unbalanced = false;
+                break;
+                
+            case 'D':
+                // horizonDisplay(avlTree, avlTree, 1);
+                printT(avlTree);
+                // puts("");
+                
+            default:
+                break;
+        }
+        puts("");
+    }
+    printf("cleaning up...");
+    freeTree(avlTree);
+    printf("\nGood bye~\n");
     return 0;
 }
