ca39f7b1c3f47cdd6fc1cf962c59bbb648c87df4
knowlet <knowlet3389@gmail.com>
2015-04-13 14:12:22 +0800

    add some function

--- main.c
+++ main.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #define MAX_VERTICES 20
 
-typedef struct node *nodePointer;
-struct node {
+typedef struct _node *nodePointer;
+typedef struct _node {
     int vertex;
+    int duration;
     nodePointer link;
-};
+} node;
 typedef struct {
     int count;
     nodePointer link;
 } hdnodes;
 hdnodes graph[MAX_VERTICES];
+int earliest[MAX_VERTICES] = {};
+int latest[MAX_VERTICES] = {};
 
-void tmpSort(hdnodes graph[], int n)
+void topSort(int n)
 {
     int i, j, k, top;
     nodePointer ptr;
@@ -30,12 +33,14 @@ void tmpSort(hdnodes graph[], int n)
             exit(EXIT_FAILURE);
         }
         else {
-            j = top;
+            j = top;    /* unstack a vertex */
             top = graph[top].count;
             printf("v%d, ", j);
             for (ptr = graph[j].link; ptr; ptr = ptr->link) {
                 /* decrease the count of the successor vertices of j */
                 k = ptr->vertex;
+                if (earliest[k] < earliest[j] + ptr->duration)
+                    earliest[k] = earliest[j] + ptr->duration;
                 --graph[k].count;
                 if (!graph[k].count) {
                     /* add vertex k to the stack */
@@ -45,9 +50,30 @@ void tmpSort(hdnodes graph[], int n)
             }
         }
 }
+nodePointer createNode(int vertex, int duration)
+{
+    nodePointer newNode;
+    newNode = malloc(sizeof(node));
+    newNode->vertex = vertex;
+    newNode->duration = duration;
+    newNode->link = NULL;
+    return newNode;
+}
+
+void insertNode(int k, int vertex, int duration)
+{
+    nodePointer prev, ptr;
+    for (prev = ptr = graph[k].link; ptr; ptr = ptr->link)
+        prev = ptr;
+    prev->link = createNode(vertex, duration);
+}
 
 int main(int argc, const char *argv[])
 {
-    
+    int i, n, edge, k, vertex, duration;
+    scanf("%d %d", &n, &edge);
+    for (i = 0; i < edge && scanf("%d %d %d", &k, &vertex, &duration); ++i)
+        insertNode(k, vertex, duration);
+    topSort(n);
     return 0;
 }
